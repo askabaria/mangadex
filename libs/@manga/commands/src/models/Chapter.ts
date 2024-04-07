b37697d48466439aa644f7feb22c277dd85edfb4
Niklas Becker <niklas.becker.dev@gmail.com>
2024-04-07 21:56:19 +0200

    dev: observe...

--- libs/@manga/commands/src/models/Chapter.ts
+++ libs/@manga/commands/src/models/Chapter.ts
@@ -11,9 +11,7 @@ export type M_Chapter = {
 
 export function listChapterImages(chapter: M_Chapter, save = false) {
   const quality = save ? "dataSaver" : "data";
-  if (!Object.hasOwn(chapter.chapter, quality)) {
-    console.error("eeh?", JSON.stringify(chapter));
-  }
+  console.log(chapter);
   return chapter.chapter[quality].map(
     (fileUrl) =>
       `${chapter.baseUrl}/${quality}/${chapter.chapter.hash}/${fileUrl}`
